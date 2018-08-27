45f86bde31412be712027010e908efdccf1937d2
t <t@localhost.localdomain>
2018-08-27 09:49:24 -0700

    Implement distance filtering.

--- query/filter.go
+++ query/filter.go
@@ -2,6 +2,7 @@ package query
 
 import (
 	"github.com/golang/glog"
+	"github.com/labstack/gommon/log"
 	"github.com/tstromberg/campwiz/result"
 )
 
@@ -11,20 +12,29 @@ func filter(c Criteria, res result.Results) result.Results {
 	var filtered result.Results
 
 	for _, r := range res {
+		glog.Infof("Filtering %s: %+v against %+v", r.Name, r, c)
+		if c.MaxDistance < int(r.Distance) {
+			glog.Infof("%s is too far (%.0f miles)", r.Name, r.Distance)
+			continue
+		}
 		if c.IncludeGroup && r.Availability[0].Group > 0 {
 			filtered = append(filtered, r)
+			log.Infof("Passes group filter: %+v", r.Name)
 			continue
 		}
 		if c.IncludeBoatIn && r.Availability[0].Boat > 0 {
 			filtered = append(filtered, r)
+			log.Infof("Passes boat filter: %+v", r.Name)
 			continue
 		}
 		if c.IncludeWalkIn && r.Availability[0].WalkIn > 0 {
 			filtered = append(filtered, r)
+			log.Infof("Passes walk-in filter: %+v", r.Name)
 			continue
 		}
 		if c.IncludeStandard && r.Availability[0].Standard > 0 {
 			filtered = append(filtered, r)
+			log.Infof("Passes standard filter: %+v", r.Name)
 			continue
 		}
 	}
