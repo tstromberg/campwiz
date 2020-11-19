e15d357518c6d7cb0d9c16b262111353aa0b1757
Thomas Stromberg <t+github@stromberg.org>
2020-11-18 21:40:38 -0800

    Update CC parser

--- cmd/update_cc/update_cc.go
+++ cmd/update_cc/update_cc.go
@@ -1,8 +1,9 @@
-// utility to update cc.yaml
+// update_cc updates CC metadata (cc.yaml) from exported HTML files
 package main
 
 import (
 	"bufio"
+	"flag"
 	"fmt"
 	"log"
 	"os"
@@ -10,16 +11,20 @@ import (
 	"strconv"
 	"strings"
 
+	"github.com/tstromberg/campwiz/pkg/mangle"
 	"github.com/tstromberg/campwiz/pkg/metadata"
 	"gopkg.in/yaml.v2"
 	"k8s.io/klog/v2"
 )
 
 var (
-	titleRe      = regexp.MustCompile(`^\d+ ([A-Z].*)`)
-	ratingRe     = regexp.MustCompile(`^Scenic rating: (\d+)`)
-	descRe       = regexp.MustCompile(`^([A-Z].*[\.\!\)])$`)
-	localeRe     = regexp.MustCompile(`^([a-z]{2,} .*[a-z])`)
+	titleRe      = regexp.MustCompile(`^<h3 class="h3_1".*<strong>(.*?)</strong></a></h3>`)
+	ratingRe     = regexp.MustCompile(`Scenic rating: (\d+)`)
+	descRe       = regexp.MustCompile(`<p class="noindent">(.*?)</p>`)
+	facilitiesRe = regexp.MustCompile(`<p class="noindent"><strong>Campsites, facilities:</strong>(.*?)</p>`)
+	reserveRe    = regexp.MustCompile(`<p class="noindent"><strong>Reservations, fees:</strong> (.*?)</p>`)
+	localeRe     = regexp.MustCompile(`^<p class="noindentt_1">(.*?)</p>`)
+	contactRe    = regexp.MustCompile(`^<p class="noindent"><strong>Contact:</strong> (.*?)</p>`)
 	maxDescWords = 50
 )
 
@@ -48,7 +53,8 @@ func key(name string, locale string) string {
 	if len(location) > 2 {
 		location = location[0:2]
 	}
-	return key + " - " + strings.Join(location, " ")
+
+	return "cc/" + strings.ToLower(location[0]) + "/" + strings.ToLower(strings.Replace(key, " ", "_", -1))
 }
 
 // parse parses text, emits entries.
@@ -61,7 +67,7 @@ func parse(scanner *bufio.Scanner) (metadata.XrefData, error) {
 		line := scanner.Text()
 		klog.V(1).Infof("Line: %s", line)
 		m := titleRe.FindStringSubmatch(line)
-		if len(m) > 0 && line == strings.ToUpper(line) {
+		if len(m) > 0 {
 			klog.V(1).Infof("Title: %s", m[1])
 			// Clear the previous entry.
 			if s.Name != "" && s.Rating > 0 {
@@ -118,25 +124,33 @@ func parse(scanner *bufio.Scanner) (metadata.XrefData, error) {
 }
 
 func main() {
-	file, err := os.Open(os.Args[1])
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer file.Close()
+	klog.InitFlags(nil)
+	flag.Parse()
 
-	scanner := bufio.NewScanner(file)
-	es, err := parse(scanner)
-	if err != nil {
-		log.Fatalf("parse error: %v", err)
+	entries := metadata.XrefData{}
+
+	for _, path := range flag.Args() {
+		f, err := os.Open(path)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		scanner := bufio.NewScanner(f)
+		es, err := parse(scanner)
+		if err != nil {
+			log.Fatalf("parse error: %v", err)
+		}
+
+		if err := scanner.Err(); err != nil {
+			log.Fatal(err)
+		}
+
+		entries.Entries = append(entries.Entries, es.Entries...)
 	}
 
-	d, err := yaml.Marshal(&es)
+	d, err := yaml.Marshal(&entries)
 	if err != nil {
 		log.Fatalf("error: %v", err)
 	}
 	fmt.Printf("%s", d)
-
-	if err := scanner.Err(); err != nil {
-		log.Fatal(err)
-	}
 }
